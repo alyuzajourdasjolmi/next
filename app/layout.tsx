9cbf78a15c69c899416f7202d788f619663bcf46
alyuzajourdasjolmi <jolmialyuza@gmail.com>
2026-04-29 11:29:49 +0700

    feat: initialize root layout with metadata and Google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./style.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,9 +13,13 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// Penambahan Meta Tag Verifikasi Google di sini
 export const metadata: Metadata = {
   title: "Hijrah Toko - Frozen Food & ATK Lengkap",
   description: "Hijrah Toko menyediakan frozen food berkualitas dan alat tulis kantor lengkap. Solusi dapur dan meja kerja dalam satu pintu.",
+  verification: {
+    google: "DiDsLBn93uY2TjOmlRwh5sWJ9Ip6VU3aDt6S3m50NfE",
+  },
 };
 
 export default function RootLayout({
